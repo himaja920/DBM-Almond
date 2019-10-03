2a48ea97a0e7fb94eb7be1eeee5cfcc7dfa69e16
Himaja Vedantam <55268846+himaja920@users.noreply.github.com>
2019-10-03 16:53:07 +0530

    Update DBM.c

--- DBM.c
+++ DBM.c
@@ -3,6 +3,12 @@
 #include <stdio.h>
 #include "libAlmondDBM.h"
 
+#define DBSIZE 25600
+#define ADD 22
+#define DEL 2
+#define EDIT 18
+#define BUFSIZE 113
+
 enum deviceType{
  UnknownDeviceType = 0,
  TemperatureSensor = 1,
@@ -24,21 +30,29 @@ enum type{
  Humidity = 7
 };
 
+enum DD {
+ID = 0,
+TYPE = 1,
+NAME = 2,
+LOCATION = 3,
+MANUFACTURER =4,
+TIME = 5
+};
 
 struct deviceData {
     int id;
-    int deviceType; 
-    char name[30];
-    char location[30];
-    char manufacturer[30];
+    int deviceType; //define some enumerations
+    char name[32];
+    char location[32];
+    char manufacturer[32];
     int lastModifiedTime;
 } ;
 
 struct dvPair {
     int type;
-    int id; 
+    int id; // to indicate the dvPair number
     char value[20];
-} ;
+};
 
 
 int main()
@@ -57,95 +71,83 @@ int main()
     printf("Enter the choice from MENU\n");
     int choice;
     scanf("%d",&choice);
-    int sp =1;
+    int sp =1,k;
    
-    depot = dpopen("DBM.bin",22,25600);
+    depot = dpopen("DBM.bin",ADD,DBSIZE);
     int rec  = dprnum(depot);
     printf(" records of depot is  %d\n",rec);
     int use = dpbusenum(depot);
     printf("used elements in record : %d\n",use);
-
+    
     if(choice == 1)
      {
-        depot = dpopen("DBM.bin",22,25600);
+        depot = dpopen("DBM.bin",ADD,DBSIZE);
         if(depot==NULL)
           { printf("File not created\n");}
         else 
-            printf("File created\n");
-
-        printf("Enter the Device ID\n");
-        scanf("%d",&Data.id);
-       
-        printf("Enter the Device Type\n");
-        scanf("%s",&Data.deviceType);
-       
+            printf("File created\n");      
         printf("Enter the Device data\n");
-        scanf("%s %s %s",&Data.name,&Data.location,&Data.manufacturer);
+        scanf(" %d",&Data.id);
+        
+        printf("Enter the Device type\n");
+        scanf("%d",&Data.deviceType);
+        printf("Enter the Device name\n");
+        scanf("%s",&Data.name);
+        printf("Enter the Device location\n");
+        scanf("%s",&Data.location);
+        printf("Enter the Device manufacturer\n");
+        scanf("%s",&Data.manufacturer);
        
-        char buf[100];
-        char *d = new char[1];
-        sprintf(d,"%d",Data.deviceType);
-        strcpy(buf,d);
-        strcat(buf,Data.name);
-        strcat(buf,Data.location);
-        strcat(buf,Data.manufacturer);
-        time_t tt =  dpmtime(depot);
-        Data.lastModifiedTime = tt;
-        printf("Last modified time is %d\n",Data.lastModifiedTime);
-        char *t = new char[8];
-        sprintf(t,"%ld",Data.lastModifiedTime);
-        strcat(buf,t);      
-             
-        char *id = new char[1];
+        char *id = new char[3];
         sprintf(id,"%d",Data.id);
         printf("id is %s\n",id);
-       
-        int k = dpput(depot,id,1,buf,97,DP_DCAT);
-        printf("k is %d\n",k);     
-       
-        char i;
+      
+        time_t tt = dpmtime(depot);
+        Data.lastModifiedTime = tt;
+        
+        char buf[BUFSIZE] ;
+        snprintf( buf,BUFSIZE, "%d %s %s %s %d",Data.deviceType, Data.name,Data.location, Data.manufacturer,Data.lastModifiedTime);
+      
+        printf("buf is %s\n",buf);
+        k = dpput(depot,id,1,buf,-1,DP_DCAT);
+        printf("k is %d\n",k); 
+
+        delete(id);
+        
         printf("Enter the value count\n");
         scanf("%d",&pair.id);
         printf("%d\n",&pair.id);
-        int h = pair.id;
+        int h = pair.id,i;
         printf("%d \n",h);
         for(i = 0; i <= h;i++)
         {        
-          char *s = new char[2];
-          strcpy(s,id);
-          strcat(s,".");
-          char * b = new char[1];
-          sprintf(b,"%d",i);
-          strcat(s,b);
-          printf(" s is %s\n",s);
-         
+          char *s = new char[7];
+          snprintf(s,7,"%d.%d",Data.id,i);
+          printf("Key value is %s\n",s);
+          printf("Enter the type\n"); 
+          scanf("%d",&pair.type);
+
           printf("Enter the pair value\n");
           scanf("%s",&pair.value);
                 
-          char buffer[100];
-          printf("Enter the type\n"); 
-          scanf("%d",pair.type);
-         
-          char *c = new char[1];
-          sprintf(c,"%d",pair.type);
-          strcpy(buffer,c);
-          strcat(buffer,pair.value);
+          char buffer[BUFSIZE];
           
-          k = dpput(depot,s,1,buffer,100,DP_DCAT);
+          snprintf(buffer,BUFSIZE," %d  %d  %s ",pair.type,i,pair.value); 
+          printf("Buffer is %s\n",buffer);         
+          k = dpput(depot,s,1,buffer,-1,DP_DCAT);
           printf("k is %d\n",k);
-        }
-         printf("Buffer is %s\n",buf);
-         int size = dpvsiz(depot, id, 1);
-         printf("Size of the entered record is %d\n",size);        
+          delete(s);
+        }      
      }   
      else if (choice == 2)
      {
           printf("Enter the Device id to be deleted\n");
           scanf("%d",&Data.id);
-          char *id = new char[1];
+          char *id = new char[3];
           sprintf(id,"%d",Data.id);
-          depot = dpopen("DBM.bin",2,25600);//2 is only writing
+          depot = dpopen("DBM.bin",DEL,DBSIZE);//2 is only writing
           int d = dpout(depot,id,1);
+          delete(id);
           printf(" d is %d\n",d);
           if(d == 0)
           {
@@ -155,56 +157,59 @@ int main()
            printf("Deleted successfully a record\n");
      }
  
-    else if(choice == 3)
+     else if(choice == 3)
      {
-          depot = dpopen("DBM.bin",18,25600);
-          int sp = 10,s,l;
-          printf("Enter the device id\n");
-          scanf("%d",&Data.id);
-          char *b = new char[1];
-          sprintf(b,"%d",Data.id);
-         
-          printf("Previous record is %s\n",dpsnaffle("DBM.bin",b,1,&sp));//'sp' specifies the pointer to a variable to which the size of the region of the return value is assigned         
-
-          printf("Enter the device data\n");
-          scanf("%d %s %s %s %d",&Data.deviceType,&Data.name,&Data.location,&Data.manufacturer,&Data.lastModifiedTime);
-          
-          char buf[100];
-          char *d = new char[1];
-          sprintf(d,"%d",&Data.deviceType);
-          strcpy(buf,d);
-          strcat(buf,Data.name);
-          strcat(buf,Data.location);
-          strcat(buf,Data.manufacturer);
-          char *a = new char[10];
-          sprintf(a, "%d", &Data.lastModifiedTime); 
-          strcat(buf,a);
-          l = dpput(depot,b,1,buf,97,DP_DOVER);
-          printf(" l is %d\n",l);
-
-          printf("Enter the device value pair to be edited\n");
-          float p;
-          scanf("%f",&p);
-
-          char *st = new char[2];
-          char buffer[100];
-          sprintf(st,"%f",p);
-
-          printf("Previous key value pairs are %s\n",dpsnaffle("DBM.bin",st,1,&sp));
-      } 
-       else if(choice == 4)
-       {
-          depot = dpopen("DBM.bin",2,25600);//2 is only writing
-          int i; int d,c=0;
-          
+          depot = dpopen("DBM.bin",EDIT,DBSIZE);
+          int i,l,start,max;
+          char *k = new char[3];
+          char *o = new char[3];
+          printf("Records present in the database :\n");
           for(i = 1; i<=rec;i++)
           {
-             char *a = new char[1];
+             snprintf(o,3,"%d",i);
+             printf("Device ID %d : %s\n",i,dpsnaffle("DBM.bin",o,1,&sp));
+          }
+          printf("Enter the device id to be edited \n");
+          scanf("%d",&Data.id);
+          char *b = new char[3];
+          sprintf(b,"%d",Data.id);
+
+          char buffer[BUFSIZE] = {0};
+          
+          printf("Enter the attribute id to be edited\n"); 
+          int id;
+          scanf("%d",&id);
+          if(id == TYPE)
+          {
+             printf("Enter the new device Type\n");
+             scanf("%d",&Data.deviceType);
+             snprintf(k,sizeof(Data.deviceType),"%d",Data.deviceType);
+             printf("Enter the starting and max values\n");
+             scanf("%d %d",&start,&max);
+             l = dpgetwb(depot,b,-1,start,max,k);
+             printf("l is %d\n",l);
+          }
+          else 
+              printf("NOt done\n");  
+           delete(b);
+           delete(k);
+           delete(o);  
+      }     
+       else if(choice == 4)
+       {
+          depot = dpopen("DBM.bin",DEL,DBSIZE);//2 is only writing
+          int i; int d;
+         
+          for(i = 1; i<=rec;i++)
+          {
+             char *a = new char[3];
              sprintf(a,"%d",i);
+            
              d = dpout(depot,a,1);
-             printf("d is %d\n",d);
-          }       
-       }     
+             printf("d for %d is %d\n",i,d);
+             delete(a); 
+          } 
+       }         
     dpclose(depot);
     return 0;
 }
